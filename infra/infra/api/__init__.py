8ea6ccfdf0f5bc28a77a7405d6906c085f2450ce
Maitreya Venkataswamy <maitreyakv@gmail.com>
2026-03-23 21:33:04 -0400

    wip: progress on image push to registry

--- infra/infra/api/__init__.py
+++ infra/infra/api/__init__.py
@@ -3,7 +3,7 @@ from pulumi_docker_build import Image, BuildContextArgs, RegistryArgs
 Image(
     "api-image",
     context=BuildContextArgs(location="../apps/api"),
-    tags=["api:latest"],
-    # registries=[RegistryArgs(address="10.0.0.42:32000")],
-    push=False,
+    tags=["10.0.0.42:32000/api:latest"],
+    registries=[RegistryArgs(address="10.0.0.42:32000")],
+    push=True,
 )
